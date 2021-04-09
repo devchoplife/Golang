2664f376dc8d1f1921ec188763e87b757bdbf86f
devchoplife <dritedurojaiye@gmail.com>
2021-04-09 22:14:09 +0100

    Added files

---
+++ VariablesAndDataTypes/var.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var number uint = 500
+
+	name = "Michael"
+	number = number + 345
+	fmt.Println("Hello World, My name is", name, "with the number", number)
+}
+
+//int
+//uint
+//float64
+//string
+//boolean
+//complex
