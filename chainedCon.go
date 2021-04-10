09a119d7b2b078c56f6d853afac9ccccbf05e550
devchoplife <dritedurojaiye@gmail.com>
2021-04-10 18:23:45 +0100

    Added Chained Conditions

--- chainedCon.go
+++ chainedCon.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := 8
+
+	val := true || false && true
+	val2 := (true || false) && !false || x < 9
+	val3 := val || false
+
+	fmt.Printf("%t, %t, %t", val, val2, val3)
 }
