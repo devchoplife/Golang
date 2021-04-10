eb2001b4c07705f2f5ca244b46eb18295e3987c6
devchoplife <dritedurojaiye@gmail.com>
2021-04-10 06:28:18 +0100

    latest Commit

--- VariablesAndDataTypes/var.go
+++ VariablesAndDataTypes/var.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	var number uint = 500
+	var name string //implicit varable declaration
+	number := 500   //explicit varaiable declaration
+	//notice that adding the colon indicates you remove the var keyword
 
 	name = "Michael"
 	number = number + 345
 	fmt.Println("Hello World, My name is", name, "with the number", number)
+	fmt.Printf("%T", number)
 }
 
 //int
