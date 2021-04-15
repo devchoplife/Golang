2e3b0f77b0d66eae3586e84f567578c5e965b1fc
devchoplife <dritedurojaiye@gmail.com>
2021-04-15 20:21:40 +0100

    Added Advanced functions

--- advFunctions.go
+++ advFunctions.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func test() {
+	fmt.Println("hello")
+}
+
+func test3(myFunc func(int) int) {
+	fmt.Println(myFunc(7))
+}
+
+func main() {
+	x := test
+	x()
+
+	//creating a function inside a function
+	//main is already a function so we will define another function here
+	test2 := func(x int) int {
+		return x * -1
+	}(8) //this calls the function inline
+	fmt.Println(test2)
+
+	test3(test2)
 }
